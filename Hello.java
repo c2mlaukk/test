03bcb5d24338a85a8429342c8bc1cd7e62bd1f77
Henrik Toivakka <henrik.toivakka@cs.tamk.fi>
2013-09-30 15:14:08 +0300

    Created endless loop. Trololo

--- Hello.java
+++ Hello.java
@@ -1,6 +1,15 @@
 public class Hello {
     public static void main(String[]args) {
         System.out.println("Hello! Kaikilla on kivaa! ");
+        endlessLoop();
+    }
+
+    public static void endlessLoop() {
+        int x = 0;
+        while(true) {
+            x++;
+            x = x*x;
+        }
     }
 
 }
